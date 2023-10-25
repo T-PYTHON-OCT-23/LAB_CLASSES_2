5e9d6d86201b62a87f6a1d1a308eee1511885fef
Bader alotaibi <baderalotaibi3@gmail.com>
2023-10-25 10:40:32 +0300

    rasie exception

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 class BankAccount:
     def __init__(self,account_holder,balance=0):
         self.__account_holder = account_holder
+        if balance < 0:
+            raise Exception("Sorry, the balance must be positive")
         self.__balance = balance
 
     def withdraw(self, amount):
+        if self.balance - amount < 0:
+            raise Exception("Sorry, not enough funds")
         self.balance -= amount
 
     def deposit(self, amount):
+        if amount <= 0:
+            raise Exception("Sorry, the amount must be positive")
         self.balance += amount
 
     def get_balance(self):
@@ -17,7 +23,6 @@ class BankAccount:
     
     
 # Path: main.py
-
 account1 = BankAccount("John Doe", 1000)
 account2 = BankAccount("Bader")
 account3 = BankAccount("Ahmad", 5000)
