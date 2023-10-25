2cdc8f5c507e124bc7ed9353d9ac00ee1de6b3af
Bader alotaibi <baderalotaibi3@gmail.com>
2023-10-25 10:49:07 +0300

    more exceptions

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 class BankAccount:
     def __init__(self,account_holder,balance=0):
         self.__account_holder = account_holder
-        if balance < 0:
-            raise Exception("Sorry, the balance must be positive")
-        self.__balance = balance
-
+        if isinstance(balance, int, float):
+            if balance < 0:
+                raise Exception("Sorry, the balance must be positive")
+            self.__balance = balance
+        else:
+            raise Exception("Sorry, the balance must be an integer or float")
     def withdraw(self, amount):
-        if self.balance - amount < 0:
-            raise Exception("Sorry, not enough funds")
-        self.balance -= amount
+        if isinstance(amount, int, float):
+            if self.balance - amount < 0:
+                raise Exception("Sorry, not enough funds")
+            self.balance -= amount
+        else:
+            raise Exception("Sorry, the amount must be an integer or float")
 
     def deposit(self, amount):
-        if amount <= 0:
-            raise Exception("Sorry, the amount must be positive")
-        self.balance += amount
-
+        if isinstance(amount, int, float):
+            if amount <= 0:
+                raise Exception("Sorry, the amount must be positive")
+            self.balance += amount
+        else:
+            raise Exception("Sorry, the amount must be an integer or float")
     def get_balance(self):
         return self.__balance
     
